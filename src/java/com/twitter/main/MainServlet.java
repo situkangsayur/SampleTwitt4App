b8586be39de92bb32c387ebc2d5fab1c3d65e273
Hendri Karisma <situkangsayur@gmail.com>
2013-12-07 10:46:49 +0700

    init commit

---
+++ src/java/com/twitter/main/MainServlet.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.twitter.main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ *
+ * @author hendri
+ */
+public class MainServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet MainServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet MainServlet at " + request.getContextPath() + "</h1>");
+
+
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("uGMLFUw9iVBQd9QnoIQYQ")
+                    .setOAuthConsumerSecret("70RTdEimZyf6ja4O941Vfr3oGmOZdqlko4gZSz4nEUY")
+                    .setOAuthAccessToken("https://api.twitter.com/oauth/request_token")
+                    .setOAuthAccessTokenSecret("https://api.twitter.com/oauth/authorize");
+            TwitterFactory tf = new TwitterFactory(cb.build());
+//        Twitter twitter = tf.getInstance();
+//        	https://api.twitter.com/oauth/access_token
+//        https://api.twitter.com/oauth/authorize
+
+            Twitter twitter = null;
+
+//            RequestToken requestToken = (RequestToken) request.getSession().getAttribute("requestToken");
+//            String verifier = request.getParameter("oauth_verifier");
+            try {
+                twitter = tf.getInstance();
+
+                twitter.updateStatus("wawawawawa... authentic");
+
+                User user = twitter.verifyCredentials();
+
+                List<Status> statuses = twitter.getHomeTimeline();
+//                System.out.println("Showing @" + user.getScreenName() + "'s home timeline.");
+//                twitter.getOAuthAccessToken(requestToken, verifier);
+//                request.getSession().removeAttribute("requestToken");
+                for (Status status : statuses) {
+//                    System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+                    out.println("@" + status.getUser().getScreenName() + " - " + status.getText() + "<br>");
+                }
+            } catch (TwitterException e) {
+                throw new ServletException(e);
+            }
+            response.sendRedirect(request.getContextPath() + "/");
+
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Dec 3, 2013, 6:37:28 PM
+    Author     : hendri
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
